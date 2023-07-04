a81e1f54bfe371d7732a0018280974242ad992c0
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-04 18:53:07 -0400

    Update API

---
+++ src/modules/Apis.js
@@ -0,0 +1,6 @@
+const recipeApi = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=American';
+const commentsApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AmOmCpR05yK1s4imyHnc/comments/';
+const likesApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AmOmCpR05yK1s4imyHnc/likes/';
+
+console.log(commentsApi, likesApi);
+export default recipeApi;

07200c7dfd957f2afe64a8634a222b420e372e53
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-04 18:52:39 -0400

    Update script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
 import logo from './images/logo.png';
+import renderMeals from './modules/HomeRender.js';
 
 const logoImg = new Image();
 logoImg.src = logo;
 document.getElementById('logo').appendChild(logoImg);
+
+renderMeals();

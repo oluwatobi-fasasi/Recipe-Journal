eff17082d2aa244b003fc4cc731a23a658bc8c57
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-04 18:51:42 -0400

    Update dist

--- src/Apis.js
+++
@@ -1,5 +0,0 @@
-const recipeApi = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=American';
-const commentsApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AmOmCpR05yK1s4imyHnc/comments/';
-const likesApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AmOmCpR05yK1s4imyHnc/likes/';
-
-export { recipeApi, commentsApi, likesApi };

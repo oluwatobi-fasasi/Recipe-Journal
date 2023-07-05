442f351449036c9bccc73f4ace42b939b727d16b
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-07-05 10:27:07 -0400

    Add likes

---
+++ src/modules/Likes.js
@@ -0,0 +1,31 @@
+import { likesApi } from "./Apis.js";
+
+const addLikes = async (id) => {
+    const response = await fetch(likesApi, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: `${id}`,
+      }),
+  
+    });
+    const data = await response.json();
+    return data;
+  };
+  
+
+  const likesGet = async () => {
+    const response = await fetch(likesApi, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+  
+    });
+    const data = await response.json();
+    return data;
+  };
+
+  export {addLikes, likesGet};

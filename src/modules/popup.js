0de2d5bcc4beff56d881527c91f4a3a868b66cd8
clarence <tegumclarence@gmail.com>
2023-07-07 16:05:17 +0100

    modify popup

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,45 +1,88 @@
-const popUpPage = document.getElementById('pop-up');
-const recipeBoard = document.getElementById('recipe-board');
+import { getComment, postComment } from './Comments.js';
+import commentCounter from './CommentCounter.js';
+
+
+const footer = document.getElementById('footer');
 const header = document.querySelector('header');
-const popUpRender = async (id) => {
-  popUpPage.innerHTML = '';
+
+const popup = async (id) => {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
   const { meals } = await response.json();
-  const popUpCard = document.createElement('section');
-  popUpCard.classList.add('pop-up-card');
-  const cancelIcon = document.createElement('p');
-  cancelIcon.innerHTML = '<span class="material-symbols-outlined cancel-icon">close</span>';
-  const popUpImgDiv = document.createElement('div');
-  popUpImgDiv.classList.add('pop-up-img');
-  const popUpImg = document.createElement('img');
-  popUpImg.src = `${meals[0].strMealThumb}`;
-  const itemName = document.createElement('p');
-  itemName.classList.add('pop-up-item-name');
-  itemName.textContent = `${meals[0].strMeal}`;
-  popUpImgDiv.appendChild(popUpImg);
-  const itemDetails = document.createElement('div');
-  itemDetails.classList.add('pop-up-item-details');
-  const detail1 = document.createElement('p');
-  detail1.textContent = `Category: ${meals[0].strCategory}`;
-  const detail2 = document.createElement('p');
-  detail2.textContent = `Area: ${meals[0].strArea}`;
-  const detail3 = document.createElement('p');
-  detail3.textContent = `Ingredient-A: ${meals[0].strIngredient1}`;
-  const detail4 = document.createElement('p');
-  detail4.textContent = `Ingredient-B: ${meals[0].strIngredient3}`;
-  itemDetails.appendChild(detail1);
-  itemDetails.appendChild(detail2);
-  itemDetails.appendChild(detail3);
-  itemDetails.appendChild(detail4);
-  popUpCard.appendChild(cancelIcon);
-  popUpCard.appendChild(popUpImgDiv);
-  popUpCard.appendChild(itemName);
-  popUpCard.appendChild(itemDetails);
-  popUpPage.appendChild(popUpCard);
-  cancelIcon.addEventListener('click', () => {
-    recipeBoard.style.display = 'grid';
-    header.style.display = 'block';
-    popUpPage.style.display = 'none';
+  const mainContainer = document.querySelector('.item-wrapper');
+
+  const popUp = document.createElement('div');
+  popUp.classList.add('pop-up');
+  const popupContainer = document.createElement('div');
+  popupContainer.classList.add('popup-container');
+  const comments = await getComment(id);
+  let commentsHTML = '';
+  if (comments.length > 0) {
+    commentsHTML = comments
+      .map((item) => `<p>${item.creation_date} - ${item.username}: ${item.comment}</p>`)
+      .join('');
+  }
+
+  const {
+    strMealThumb, strMeal, strArea, strIngredient5, strCategory, strIngredient7,
+  } = meals[0];
+
+  popupContainer.innerHTML = `
+    <button class="close-btn">&times;</button>
+    <img src="${strMealThumb}" class="popup-image">
+    <h2 class="popup-food-name">${strMeal}</h2>
+    <div class="popup-item-details-container">
+    <p>Category: ${strCategory}</p>
+      <p>Area: ${strArea}</p>
+      <p>Ingredient A: ${strIngredient5}</p>
+      <p>Ingredient B: ${strIngredient7}</p>
+    </div>
+
+    <h3 class="comments-title">Comments<span class='comment-counter' id="comment-counter">(${comments.length})</span></h3>
+    <div class="comments-div">
+      ${commentsHTML}
+    </div>
+
+    <h3 class="form-title">Add a comment</h3>
+    <form class="form">
+      <input class="user-name" type="text" placeholder="Your Name" required>
+      <textarea class="your-insight" placeholder="Your Insight" cols="40" rows="5" required></textarea>
+      <button type="submit" class="submit-btn">Comment</button>
+    </form>
+  `;
+
+  
+  footer.classList.toggle('hidden');
+  header.classList.toggle('hidden');
+
+  popUp.classList.toggle('visible');
+  popUp.appendChild(popupContainer);
+  mainContainer.appendChild(popUp);
+
+  const closeBtn = popUp.querySelector('.close-btn');
+  closeBtn.addEventListener('click', () => {
+    popUp.classList.toggle('visible');
+    document.querySelector('footer').classList.toggle('hidden');
+    document.querySelector('header').classList.toggle('hidden');
+  });
+
+  const form = popupContainer.querySelector('.form');
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const userInput = form.querySelector('.user-name');
+    const userComment = form.querySelector('.your-insight');
+    await postComment(id, userInput.value, userComment.value);
+    await commentCounter(id);
+    form.reset();
+    const updatedComments = await getComment(id);
+    const commentsDiv = popupContainer.querySelector('.comments-div');
+    commentsDiv.innerHTML = updatedComments
+      .map((item) => `<p>${item.creation_date} - ${item.username}: ${item.comment}</p>`)
+      .join('');
   });
 };
-export default popUpRender;
+
+
+   
+  
+
+export default popup;

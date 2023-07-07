5a76b32ebfa86b41d18056371349523525058717
Clarence Atim <125105700+ClarenceAtim@users.noreply.github.com>
2023-07-07 23:11:26 +0000

    fix linters

--- src/modules/Likes.js
+++ src/modules/Likes.js
@@ -1,35 +1,32 @@
 import showLikesCount from './ShowLikes.js';
 
 const postLikes = async (id, board) => {
-    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AmOmCpR05yK1s4imyHnc/likes/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "item_id": `${id}`,
-        action: 'like',
-      }),
-    });
-    if (response.ok) {
-      showLikesCount(id, board);
-    } else {
-      console.error('Failed');
-    }
-
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AmOmCpR05yK1s4imyHnc/likes/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: `${id}`,
+      action: 'like',
+    }),
+  });
+  if (response.ok) {
+    showLikesCount(id, board);
+  } else {
+    console.error('Failed');
+  }
 };
-
 
 const getLikes = async () => {
-    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AmOmCpR05yK1s4imyHnc/likes/', {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    const data = await response.json();
-    return data;
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AmOmCpR05yK1s4imyHnc/likes/', {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const data = await response.json();
+  return data;
 };
 
-
-export {getLikes, postLikes};
+export { getLikes, postLikes };
